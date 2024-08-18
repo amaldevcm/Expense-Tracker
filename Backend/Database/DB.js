dd20fc0b03b5ba411ec7b23f675a9d7e09b2fcba
amaldevcm <amald1101@gmail.com>
2024-08-18 00:01:29 -0400

    Updated database connection

--- Backend/Database/DB.js
+++ Backend/Database/DB.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const db = mongoose.connect('mongodb://127.0.0.1:27017/ExpenseTracker')
+const url = 'mongodb+srv://amald1101:AmalDev%40123@cluster0.d41ro.mongodb.net/ExpenseTracker';
+// const url = 'mongodb://127.0.0.1:27017/ExpenseTracker';
+const db = mongoose.connect(url)
                     .then(() => console.log('Database Connected!!!'))
                     .catch(err => console.log(err));
 
