72d129801af4bb2f48bf4cf49b17947fd3843823
Amal Dev Cheepra Mailapravan <43089089+amaldevcm@users.noreply.github.com>
2025-07-02 13:23:47 -0400

    Update DB.js

--- Backend/Database/DB.js
+++ Backend/Database/DB.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb+srv://amald1101:AmalDev%40123@cluster0.d41ro.mongodb.net/ExpenseTracker';
+const url = 'your DB URL';
 // const url = 'mongodb://127.0.0.1:27017/ExpenseTracker';
 const db = mongoose.connect(url)
                     .then(() => console.log('Database Connected!!!'))
